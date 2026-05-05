e0af7d66b06f031f16cd6ba3b871cd81f851d07d
yuseongchae <chaeyuseong@naver.com>
2026-05-05 17:02:15 +0900

    Spatial computing - Ch3. Volumns in VisionOS

---
+++ SpatialComputing/SeaCreatures/SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by 채유성 on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
